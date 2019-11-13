356a0407d5bd71a6f8fb52c5a3f6fc1077e43fdb
Emil Ibatullin <cawabunga@users.noreply.github.com>
2019-11-13 17:56:09 +0300

    Revert "add MSCSSMatrix to matrixFn options"

--- src/utils.js
+++ src/utils.js
@@ -122,7 +122,7 @@ function matrix(el, selfOnly) {
 		} while (!selfOnly && (el = el.parentNode));
 	}
 
-	const matrixFn = window.DOMMatrix || window.WebKitCSSMatrix || window.CSSMatrix || window.MSCSSMatrix;
+	const matrixFn = window.DOMMatrix || window.WebKitCSSMatrix || window.CSSMatrix;
 	/*jshint -W056 */
 	return matrixFn && (new matrixFn(appliedTransforms));
 }
